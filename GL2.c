ada10282933c439a3b3a3d7c533d0ddf7aee6c3a
Microsxema <kirill222@email.ru>
2019-01-11 00:47:59 +0700

    уборка в коде

--- GL2.c
+++ GL2.c
@@ -4,92 +4,24 @@
 #include <termios.h>
 #include <unistd.h>
 #include <math.h>
+#define WIDTH 400  //ширина
+#define HEIGHT 400 //длинна
+#define DEPTH 400  //глубина
 
 
-int a = 90;
-double z_step = -200;
-double x_step = 100;
-double z_turn = 1;
-double x_turn = 0;
 
 
-void normalKeys(unsigned char key, int x, int y)
+int a = 90; 		 	//угол поворота
+double z_begin = -200;	//начало камеры z
+double x_begin = 100;	//начало камеры x
+double z_turn = 1;		//поворот камеры z
+double x_turn = 0;		//поворот камеры z
+
+
+
+
+void drawing3D()
 {
-	if(key == 'a') a += 3;	
-	
-	else if(key == 'd') a -= 3;
-	
-	else if(key == 'w')
-	{
-		z_step += z_turn * 5; 
-		x_step -= x_turn * 5;
-	}
-	
-	else if(key == 's')
-	{
-		z_step -= z_turn * 5; 
-		x_step += x_turn * 5;
-	}
-		
-	
-	z_turn = sin(a * M_PI / 180);
-	x_turn = cos(a * M_PI / 180);
-
-	glutPostRedisplay();
-}
-
-
-void reshpe(int x, int y)
-{	
-	int z = x;
-	 
-	glViewport(0, 0, x, y);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(60, x/y, 0.1, 1000);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-}
-
-
-void drawAim(void)
-{
-	glMatrixMode(GL_PROJECTION); 
-	glLoadIdentity(); 
-	glOrtho(0, 10, 10, 0, -1, 1); 
-
-	glMatrixMode(GL_MODELVIEW); 
-	glLoadIdentity(); 
-	
-	glBegin(GL_LINES);
-		glColor3f(1, 1, 1); 
-		glVertex2f(5, 4.8); 
-		glVertex2f(5, 5.2);   
-	glEnd();
-	//
-	glBegin(GL_LINES);
-		glColor3f(1, 1, 1); 
-		glVertex2f(4.8, 5); 
-		glVertex2f(5.2, 5);   
-	glEnd();  
-}
-
-
-
-void pl(void)
-{	
-	reshpe(400, 400);
-
-	glLoadIdentity();
-	
-	gluLookAt(x_step, -250,  z_step,
-			  x_step - x_turn, -250,  z_step + z_turn,
-			  0, 1, 0
-	);
-	
-	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-	
-	
 	//ось y
 	glBegin(GL_LINES);
 		glColor3f(0.4, 0.1, 0.8);
@@ -183,20 +115,86 @@ void pl(void)
 	  glVertex3f( 80, -250, -80);
 	  glVertex3f(-80, -250, -80);
 	glEnd();
+}
+
+
+void keyboard_handling(unsigned char key, int x, int y)
+{
+	if(key == 'a') a += 3;	
 	
-	glTranslatef(-60, 0, -400);
+	else if(key == 'd') a -= 3;
 	
+	else if(key == 'w')
+	{
+		z_begin += z_turn * 5; 
+		x_begin -= x_turn * 5;
+	}
 	
-	glBegin(GL_POLYGON);
-	  glColor3f(0.0, 0.1, 0.8);
-	  glVertex3f(-80, -250,  400);
-	  glVertex3f(-80, -300,  400);
-	  glVertex3f(-70, -300,  400);
-	  glVertex3f(-70, -250,  400);
+	else if(key == 's')
+	{
+		z_begin -= z_turn * 5; 
+		x_begin += x_turn * 5;
+	}
+		
+	
+	z_turn = sin(a * M_PI / 180);
+	x_turn = cos(a * M_PI / 180);
+
+	glutPostRedisplay();
+}
+
+
+void perspective(int w, int h)
+{	
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60, w/h, 0.1, 1000);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+
+void drawing_cursor(void)
+{
+	glMatrixMode(GL_PROJECTION); 
+	glLoadIdentity(); 
+	glOrtho(0, 10, 10, 0, -1, 1); 
+
+	glMatrixMode(GL_MODELVIEW); 
+	glLoadIdentity(); 
+	
+	glBegin(GL_LINES);
+		glColor3f(1, 1, 1); 
+		glVertex2f(5, 4.8); 
+		glVertex2f(5, 5.2);   
 	glEnd();
+	//
+	glBegin(GL_LINES);
+		glColor3f(1, 1, 1); 
+		glVertex2f(4.8, 5); 
+		glVertex2f(5.2, 5);   
+	glEnd();  
+}
+
+
+
+void scene(void)
+{	
+	perspective(WIDTH, HEIGHT);
+
+	glLoadIdentity();
 	
+	gluLookAt(x_begin, 			-250,  z_begin,
+			  x_begin - x_turn, -250,  z_begin + z_turn,
+			  0, 1, 0
+	);
 	
-	drawAim();
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	
+	drawing3D();
+	
+	drawing_cursor();
 	
 	glutSwapBuffers();
 }
@@ -206,13 +204,12 @@ int main(int argc, char* argv[])
 {	
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(400, 400);
-	glutCreateWindow("Awesome Cube");
+	glutInitWindowSize(WIDTH, HEIGHT);
 	glEnable(GL_DEPTH_TEST);
+	glutCreateWindow("Walk in 3D");
 
-	glutDisplayFunc(pl);
-	//glutReshapeFunc(reshpe);
-	glutKeyboardFunc(normalKeys);
+	glutDisplayFunc(scene);
+	glutKeyboardFunc(keyboard_handling);
 	
 	glutMainLoop();
 }
